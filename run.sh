07183f65b3450f8f1cc5f0d8638d8a0c17a690ae
mtracy <mitchell.tracy@hpe.com>
2017-08-20 13:26:11 -0400

    fixed typo

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-source confiv.env
+source config.env
 docker build --build-arg APP_NAME=$APP_NAME -t $APP_NAME .
 docker run -it -v $PWD:/usr/src/$APP_NAME --rm --name $CONTAINER_NAME $APP_NAME
